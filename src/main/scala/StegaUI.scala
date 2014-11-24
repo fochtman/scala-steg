0f47636d91f156ecdaab1ed8289063f8489a1bb3
Tyler Fochtman <tlfochtman@gmail.com>
2014-11-23 23:44:35 -0600

    cleaning up UI

--- src/main/scala/StegaUI.scala
+++ src/main/scala/StegaUI.scala
@@ -10,8 +10,8 @@ object StegaUI extends SimpleSwingApplication {
   val chooser = new FileChooser
 
 
-  val plainText = new TextArea(20, 60) { editable = false }
-  val encodedText = new TextArea(20, 60) { editable = false }
+  val plainText = new TextArea(20, 40) { editable = false }
+  val encodedText = new TextArea(20, 40) { editable = false }
   val inputText = new TextField {
     text = "Enter message here"
     listenTo(keys)
@@ -30,13 +30,10 @@ object StegaUI extends SimpleSwingApplication {
     val mimickerCodeTree = Huffman.createCodeTree(inputCharList)(Huffman.singleton)
     val mimickerBits = Huffman.quickEncode(mimickerCodeTree)(inputCharList)
 
-    //encodedText.text = mimickerBits.toString
-
-    val a = Huffman.decode(mimickedCodeTree, mimickerBits).mkString
-    val b = "\n"*5 + Huffman.decodedSecret.mkString
-    encodedText.text = a + b + "\n"*5 + Huffman.charByWeightAndEncoding(xs)
-    //val encoderFunc = Huffman.quickEncode(mimickedCodeTree)_
-    //encodedText.text = encoderFunc(inputText.text.toList).mkString.sliding(32, 32).toList.mkString("\n") + "\n"*5 + Huffman.decodedSecret.mkString
+    val msg = Huffman.decode(mimickedCodeTree, mimickerBits).mkString
+    val txtStatistics = Huffman.charByWeightAndEncoding(xs)
+    encodedText.text = msg + "\n\n" + txtStatistics
+    encodedText.caret.position = 0
   }
 
   def open(): Unit = {
@@ -46,14 +43,16 @@ object StegaUI extends SimpleSwingApplication {
       src.close()
 
       plainText.text = lines.mkString("\n")
+      plainText.caret.position = 0
+
       xs = lines.mkString.trim.filter(c => c.isLetterOrDigit || c == ' ').toLowerCase.toList
       writeEncoded()
     }
   }
 
   def fileMenu = new Menu("File") {
-    contents += new MenuItem(Action("Open") { open() })
-    contents += new MenuItem(Action("Exit") { sys.exit(0) })
+    contents += new MenuItem(Action("Open") {open()})
+    contents += new MenuItem(Action("Exit") {sys.exit(0)})
   }
 
   def top = new MainFrame {
@@ -63,14 +62,9 @@ object StegaUI extends SimpleSwingApplication {
       contents += fileMenu
     }
 
-    case class CustomScroll(txtArea: TextArea) extends ScrollPane(txtArea) {
-      horizontalScrollBarPolicy = ScrollPane.BarPolicy.AsNeeded
-    }
-
     contents = new BorderPanel {
       layout(new ScrollPane(plainText)) = East
       layout(new ScrollPane(encodedText)) = West
-      layout(CustomScroll(encodedText)) = West
       layout(new ScrollPane(inputText)) = South
     }
   }
