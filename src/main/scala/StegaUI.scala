97289883ab3fc81f0f7a318ccea8e5869dd167d9
fochtman <tlfochtman@gmail.com>
2014-11-26 21:33:02 -0800

    update ui
    users can now import a file, look for a seed to the mimic function, then
    a message to encode

--- src/main/scala/StegaUI.scala
+++ src/main/scala/StegaUI.scala
@@ -1,3 +1,5 @@
+import java.awt.FlowLayout
+
 import scala.swing._
 import scala.swing.BorderPanel.Position._
 import javax.swing.ScrollPaneConstants._
@@ -9,11 +11,17 @@ object StegaUI extends SimpleSwingApplication {
 
   val chooser = new FileChooser
 
-
   val plainText = new TextArea(20, 40) { editable = false }
   val encodedText = new TextArea(20, 40) { editable = false }
-  val inputText = new TextField {
-    text = "Enter message here"
+
+  val inputSeed = new TextField {
+    columns = 75
+    text = "select substring from loaded text"
+  }
+
+  val inputMsg = new TextField {
+    columns = 75
+    text = "type msg here, then hit enter"
     listenTo(keys)
     reactions += {
       case KeyPressed(_, Key.Enter, _, _) =>
@@ -21,37 +29,41 @@ object StegaUI extends SimpleSwingApplication {
     }
   }
 
-  var xs: List[Char] = Nil
+  val msgPanel = new FlowPanel(FlowPanel.Alignment.Center)(new Label("MSG: "), inputMsg)
+  val seedPanel = new FlowPanel(FlowPanel.Alignment.Center)(new Label("SEED: "), inputSeed)
+  val inputPanel = new BorderPanel {
+    layout(seedPanel) = North
+    layout(msgPanel) = South
+  }
+
+  //var xs: List[Char] = Nil
+  var xs: List[String] = Nil
 
   // takes typed input and translates it into msg
   def getMsg(mimickedCodeTree: Huffman.CodeTree): String = {
-    val inputCharList = inputText.text.toList
+    val inputCharList = inputMsg.text.toList
     val mimickerCodeTree = Huffman.createCodeTree(inputCharList)(Huffman.singleton)
     val mimickerBits = Huffman.quickEncode(mimickerCodeTree)(inputCharList)
     Huffman.decode(mimickedCodeTree, mimickerBits).mkString
   }
 
   def writeEncoded(): Unit = {
+    /*
     val mimickedCodeTree = Huffman.createCodeTree(xs)(Huffman.singleton)
-
     val msg = getMsg(mimickedCodeTree)
-
     val txtStatistics = Huffman.charByWeightAndEncoding(xs)
+    */
 
     // exp. w/ mimicfunctions
-    val sst = Wayner.nthOrderFrequencies(5, xs.mkString)
+    val sst = Wayner.nthOrderFrequencies(10, xs.mkString("\n"))
     val mf = Wayner.createMimicFunction(sst)
-    val mfStr = mf.mkString("\n")
 
     // use mimic functions
-    val bits = Wayner.createBitList(inputText.text)
-    val seed = "Venus"
-    //val seed = "with closed eyes"
-    //val seed = "Say first"
-    //val seed = "#include"
+    val bits = Wayner.createBitList(inputMsg.text)
+    val seed = inputSeed.text
     val resu = Wayner.encode(seed, bits, mf)
 
-    encodedText.text = (resu.split(" ")).toList.sliding(4, 4).map(_.mkString(" ")).mkString("\n")
+    encodedText.text = resu
     //encodedText.text = msg + "\n\n" + txtStatistics + "\n\n" + mfStr
     encodedText.caret.position = 0
   }
@@ -65,9 +77,8 @@ object StegaUI extends SimpleSwingApplication {
       plainText.text = lines.mkString("\n")
       plainText.caret.position = 0
 
-      //xs = lines.mkString.trim.filter(c => c.isLetterOrDigit || c == ' ').toLowerCase.toList
-      xs = lines.mkString.trim.toList
-      writeEncoded()
+      //xs = lines.mkString.trim.toList
+      xs = lines
     }
   }
 
@@ -86,7 +97,8 @@ object StegaUI extends SimpleSwingApplication {
     contents = new BorderPanel {
       layout(new ScrollPane(plainText)) = East
       layout(new ScrollPane(encodedText)) = West
-      layout(new ScrollPane(inputText)) = South
+      //layout(new ScrollPane(inputText)) = South
+      layout(inputPanel) = South
     }
   }
 }
